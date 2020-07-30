648b4f09a4609a28de57f4147a3bdaa6f40814be
chenxinxiner <1074537862@qq.com>
2020-07-30 23:17:14 +0800

    feat: 项目初创建

---
+++ logprocess-es/src/main/java/com/chenxinzouzou/logprocesselasticsearch/LogprocessElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.chenxinzouzou.logprocesselasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogprocessElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogprocessElasticsearchApplication.class, args);
+    }
+
+}
